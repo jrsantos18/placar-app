74292c51e29604d6b45255aa607eeeb99382f019
Jose Messias Jr <jrsantos18@gmail.com>
2020-05-20 17:07:26 -0300

    M6A6 - Implementando a lógica do placar

--- app/src/main/java/com/josemessiasjunior/placarapp/MainActivity.kt
+++ app/src/main/java/com/josemessiasjunior/placarapp/MainActivity.kt
@@ -2,11 +2,25 @@ package com.josemessiasjunior.placarapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var scoreTime1 = 0 //int
+        var scoreTime2 = 0
+
+        scoreTime1TXT.setOnClickListener {
+            scoreTime1 = scoreTime1 + 1
+            scoreTime1TXT.text = scoreTime1.toString()
+        }
+
+        scoreTime2TXT.setOnClickListener {
+            scoreTime2 = scoreTime2 + 1
+            scoreTime2TXT.text = scoreTime2.toString()
+        }
     }
 }
